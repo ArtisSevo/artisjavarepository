8ee5ac8f0ff1361e573cccc500e3bedbb624ea6b
Artis Sevostjanovs <seja1@inbox.lv>
2019-11-12 21:07:18 +0200

    First Itellij commit

---
+++ src/testmain.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class testmain {
+    public static void main(String[] args) {
+        var a = new int[]{2, 45, 555, 54};
+
+        public static int findMode ( int[] array){
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+
+            public static int mediana(int[] array);
+
+        }
+
+        System.out.println();
+    }
+}
