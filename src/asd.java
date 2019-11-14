85fa7368b117d4a880d81012c52060cb000dd8d7
Artis Sevostjanovs <seja1@inbox.lv>
2019-11-14 20:49:12 +0200

    First Itellij commit

---
+++ src/asd.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException
+import java.util.Scanner;
+
+public class asd {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+        String visitorName = scanner.nextLine();
+
+    }
+    public static void logVisitor(String visitor)  throw IOException {
+        FileWriter visitorFile = new FileWriter()pathname:"visitor.log");
+
+        fileWriter.write(visitor);
+
+        fileWriter.close();
+
+    }
+
+    
+
+}
+
+
